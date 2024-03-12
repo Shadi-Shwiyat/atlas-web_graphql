56baa54a139595716800fb6e78a909a5b933b672
Shadi-Shwiyat <raz611103@gmail.com>
2024-03-12 17:09:37 -0500

    task 4 creating type relations between task and project

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -5,29 +5,75 @@ const { GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLID,
+  GraphQLList,
   GraphQLSchema } = require('graphql');
 
 const lodash = require('lodash');
 
-// Define TaskType
-const TaskType = new GraphQLObjectType({
-  name: 'Task',
-  fields: {
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-    description: { type: GraphQLString },
+const tasks = [
+  {
+    id: '1',
+    projectID: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
   },
-});
+  {
+    id: '2',
+    projectID: '1',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+]
 
+const projects = [
+  {
+    id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
+  },
+  {
+    id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
+  },
+]
+
+// Define ProjectType
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLID},
     title: {type: GraphQLString},
     weight: {type: GraphQLInt},
     description: {type: GraphQLString},
-  }
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return lodash.filter(tasks, {projectID: parent.id});
+      }
+    }
+  }),
+});
+
+// Define TaskType
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: () => ({
+    id: { type: GraphQLID },
+    project: {
+      type: ProjectType,
+      resolve(parent, args) {
+        return lodash.find(projects, {id: parent.projectID});
+      }
+    },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  }),
 });
 
 // Define RootQuery
@@ -40,21 +86,6 @@ const RootQueryType = new GraphQLObjectType({
         id: { type: GraphQLID }, 
       },
       resolve(parent, args) {
-        const tasks = [
-          {
-            id: '1',
-            title: 'Create your first webpage',
-            weight: 1,
-            description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-          },
-          {
-            id: '2',
-            title: 'Structure your webpage',
-            weight: 1,
-            description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-          },
-        ]
-
         return lodash.find(tasks, {'id': args.id});
       },
     },
@@ -64,21 +95,6 @@ const RootQueryType = new GraphQLObjectType({
         id: { type: GraphQLID }, 
       },
       resolve(parent, args) {
-        const projects = [
-          {
-            id: '1',
-            title: 'Advanced HTML',
-            weight: 1,
-            description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
-          },
-          {
-            id: '2',
-            title: 'Bootstrap',
-            weight: 1,
-            description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
-          },
-        ]
-
         return lodash.find(projects, {'id': args.id});
       },
     },

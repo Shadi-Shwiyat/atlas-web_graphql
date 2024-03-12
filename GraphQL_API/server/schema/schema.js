bcc4e756646e78ea4b122824b435fe7daf1c4b42
Shadi-Shwiyat <raz611103@gmail.com>
2024-03-12 16:23:17 -0500

    task 2 finishing resolve function and writing a graphql query

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -3,6 +3,8 @@
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
+const lodash = require('lodash');
+
 // Define TaskType
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -23,7 +25,23 @@ const RootQueryType = new GraphQLObjectType({
       args: {
         id: { type: GraphQLString }, 
       },
-      resolve(parent, args) {},
+      resolve(parent, args) {
+        const tasks = [
+          {
+            id: '1',
+            title: 'Create your first webpage',
+            weight: 1,
+            description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+          },
+          {
+            id: '2',
+            title: 'Structure your webpage',
+            weight: 1,
+            description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+          },
+        ]
+        return lodash.find(tasks, {'id': args.id});
+      },
     },
   },
 });
